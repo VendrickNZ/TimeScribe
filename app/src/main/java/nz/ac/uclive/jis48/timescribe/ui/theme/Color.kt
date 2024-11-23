ce27c7d683b761e08fd977a091b1f7727087ba01
Jakib Isherwood <jakibisherwood88@gmail.com>
2024-11-23 14:27:20 +1300

    Updated visual theme/colours.

--- app/src/main/java/nz/ac/uclive/jis48/timescribe/ui/theme/Color.kt
+++ app/src/main/java/nz/ac/uclive/jis48/timescribe/ui/theme/Color.kt
@@ -2,20 +2,39 @@ package nz.ac.uclive.jis48.timescribe.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val lightRed = Color(0xFFf23743)
-val darkRed = Color(0xFFbf0710)
-val lightBlue = Color(0xFF73dcf9)
-val darkerLightBlue = Color(0xFF09addb)
-val darkBlue = Color(0xFF440ec8)
-val green = Color(0xFF00C853)
-val darkGreen = Color(0xFF007E33)
-val softBlue = Color(0xFF0077CC)
-val yellow = Color(0xFFe6ff22)
-val darkerBlue = Color(0xFF005599)
-val lightPurple = Color(0xffad39f1)
-var darkPurple = Color(0xFF3700B3)
-val teal = Color(0xFF03DAC6)
-val white = Color(0xFFFFFFFF)
-val black = Color(0xFF000000)
-val gray = Color(0xFF808080)
-val darkGray = Color(0xFF303030)
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+
+val LightPrimary = Color(0xFFF44336)         // Material Red 500
+val LightPrimaryVariant = Color(0xFFD32F2F)   // Material Red 700
+val LightSecondary = Color(0xFF03A9F4)        // Material Light Blue 500
+val LightSecondaryVariant = Color(0xFF0288D1) // Material Light Blue 700
+val LightBackground = White
+val LightSurface = White
+val LightOnPrimary = White
+val LightOnSecondary = White
+val LightOnBackground = Black
+val LightOnSurface = Black
+
+val DarkPrimary = Color(0xFFEF9A9A)          // Light Red 300
+val DarkPrimaryVariant = Color(0xFFE57373)    // Light Red 400
+val DarkSecondary = Color(0xFF81D4FA)         // Light Blue 300
+val DarkSecondaryVariant = Color(0xFF4FC3F7)  // Light Blue 400
+val DarkBackground = Color(0xFF121212)
+val DarkSurface = Color(0xFF1E1E1E)
+val DarkOnPrimary = Black
+val DarkOnSecondary = Black
+val DarkOnBackground = White
+val DarkOnSurface = White
+
+val ErrorColor = Color(0xFFB00020)
+val OnErrorColor = White
+
+val WorkColorLight = Color(0xFFF44336) // Red 500
+val WorkColorDark = Color(0xFFEF9A9A)  // Red 200
+
+val BreakColorLight = Color(0xFF2196F3) // Blue 500
+val BreakColorDark = Color(0xFF90CAF9)  // Blue 200
+
+val LongBreakColorLight = Color(0xFF4CAF50) // Green 500
+val LongBreakColorDark = Color(0xFFA5D6A7)  // Green 200
--- app/src/main/java/nz/ac/uclive/jis48/timescribe/ui/theme/Theme.kt
+++ app/src/main/java/nz/ac/uclive/jis48/timescribe/ui/theme/Theme.kt
@@ -1,39 +1,46 @@
 package nz.ac.uclive.jis48.timescribe.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 @Composable
-fun TimeScribeTheme(darkModeState: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+fun TimeScribeTheme(
+    darkModeState: Boolean,
+    content: @Composable () -> Unit
+) {
     val darkColorPalette = darkColors(
-        primary = softBlue,
-        primaryVariant = darkerBlue,
-        secondary = yellow,
-        background = black,
-        surface = black,
-        onPrimary = white,
-        onSecondary = black
+        primary = DarkPrimary,
+        primaryVariant = DarkPrimaryVariant,
+        secondary = DarkSecondary,
+        secondaryVariant = DarkSecondaryVariant,
+        background = DarkBackground,
+        surface = DarkSurface,
+        error = ErrorColor,
+        onPrimary = DarkOnPrimary,
+        onSecondary = DarkOnSecondary,
+        onBackground = DarkOnBackground,
+        onSurface = DarkOnSurface,
+        onError = OnErrorColor
     )
 
     val lightColorPalette = lightColors(
-        primary = lightPurple,
-        primaryVariant = darkPurple,
-        secondary = teal,
-        background = white,
-        surface = white,
-        onPrimary = white,
-        onSecondary = black
+        primary = LightPrimary,
+        primaryVariant = LightPrimaryVariant,
+        secondary = LightSecondary,
+        secondaryVariant = LightSecondaryVariant,
+        background = LightBackground,
+        surface = LightSurface,
+        error = ErrorColor,
+        onPrimary = LightOnPrimary,
+        onSecondary = LightOnSecondary,
+        onBackground = LightOnBackground,
+        onSurface = LightOnSurface,
+        onError = OnErrorColor
     )
 
-
-    val colors = if (darkModeState) {
-        darkColorPalette
-    } else {
-        lightColorPalette
-    }
+    val colors = if (darkModeState) darkColorPalette else lightColorPalette
 
     MaterialTheme(
         colors = colors,
