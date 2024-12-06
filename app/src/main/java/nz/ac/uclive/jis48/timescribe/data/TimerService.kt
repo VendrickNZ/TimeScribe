678f4fb5b7edd589ed687513fdb55b6f822971fd
Jakib Isherwood <jakibisherwood88@gmail.com>
2024-12-06 16:36:12 +1300

    Removed unused import.

--- app/src/main/java/nz/ac/uclive/jis48/timescribe/data/TimerService.kt
+++ app/src/main/java/nz/ac/uclive/jis48/timescribe/data/TimerService.kt
@@ -3,7 +3,6 @@ package nz.ac.uclive.jis48.timescribe.data
 import android.app.Notification
 import android.app.NotificationManager
 import android.app.Service
-import android.content.Context
 import android.content.Intent
 import android.os.*
 import androidx.core.app.NotificationCompat
