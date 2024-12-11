8a38162d6444ff5d82876673ff0b5c7ff41607d4
Jakib Isherwood <jakibisherwood88@gmail.com>
2024-12-11 18:44:57 +1300

    Removed unused val.

--- app/src/main/java/nz/ac/uclive/jis48/timescribe/data/SettingsRepository.kt
+++ app/src/main/java/nz/ac/uclive/jis48/timescribe/data/SettingsRepository.kt
@@ -13,7 +13,7 @@ import kotlinx.coroutines.flow.map
 
 val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 
-class SettingsRepository(private val context: Context) {
+class SettingsRepository(context: Context) {
     private val dataStore: DataStore<Preferences> = context.dataStore
     companion object {
         val WORK_DURATION_KEY = intPreferencesKey("work_duration")
